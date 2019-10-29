86022cbf77c3af70259f876bc1290e0d3afeaebb
DiegoGonzalez87 <alejnadrodga87@gmail.com>
2019-10-29 13:31:37 -0500

    Version5

--- src/keys.js
+++ src/keys.js
@@ -1,6 +1,6 @@
 module.exports = {
     db:{
-        host: 'shared12.hostgator.co',
+        host: '162.241.2.168',
         user: 'kayssens',
         password: 'd13g0g0n?',
         database: 'kayssens_kayssen'
